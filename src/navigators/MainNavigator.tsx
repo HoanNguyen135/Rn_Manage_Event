d82b46051d5d6823ffbd8ba0fcd9b220f07d1acb
HoanNguyen <hoannguyen13520@gmail.com>
2024-06-03 17:25:05 +0700

    add package async stoge

--- src/navigators/MainNavigator.tsx
+++ src/navigators/MainNavigator.tsx
@@ -2,6 +2,7 @@ import {View, Text} from 'react-native';
 import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import TabNavigator from './TabNavigator';
+import SCREEN_NAME from '../utils/screenName';
 
 const MainNavigator = () => {
   const Stack = createNativeStackNavigator();
